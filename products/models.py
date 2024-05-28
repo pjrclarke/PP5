c886be324152c815e0011099837ac4f0d3eade42
pjrclarke <pauljrclarke@gmail.com>
2024-05-28 16:23:37 +0100

    product detail page added

--- products/models.py
+++ products/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(null=True, blank=True, upload_to='media/product_images/')
+    image = models.ImageField(null=True, blank=True, upload_to='product_images/')
 
     def __str__(self):
         return self.name
@@ -34,7 +34,7 @@ def product_image_upload_path(instance, filename):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to= 'media/product_images')
+    image = models.ImageField(upload_to="product_images/")
 
     def __str__(self):
         return f"Image for {self.product.name}"
