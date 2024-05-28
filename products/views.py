301ae1154c11697b886799acf169f31c5f6fbfbb
pjrclarke <pauljrclarke@gmail.com>
2024-05-28 18:32:32 +0100

    added sort and filter function

--- products/views.py
+++ products/views.py
@@ -1,11 +1,53 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import *
 
+
 def all_products(request):
-    
     products = Product.objects.all()
+    query = None
+    categories = None 
+    sort = None
+    direction = None
+    
+    if request.GET:
+        if 'sort' in request.GET:
+            sortKey = request.GET['sort']
+            sort = sortKey
+            if sortKey == 'name':
+                sortKey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+                
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction =='desc':
+                    sortKey = f'-{sortKey}'
+            products = products.order_by(sortKey)
+                
+                
+                 
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+        
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+            
+    current_sorting = f'{sort}_{direction}'
+    
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
     
     return render(request, 'products/products.html', context)
