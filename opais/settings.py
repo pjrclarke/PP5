f84f89a32231b72b0101675cf4957fe19436b96a
pjrclarke <pauljrclarke@gmail.com>
2024-05-29 09:23:10 +0100

    debug on s3

--- opais/settings.py
+++ opais/settings.py
@@ -162,22 +162,22 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME: 'opais-db0374dd611e.herokuapp.com'
-    AWS_S3_REGION_NAME: 'us-east-1'
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'opais-db0374dd611e.herokuapp.com'
+    AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    
-    
+
+    # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
-
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
